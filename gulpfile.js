0dbb382367e78f5325c36e381181602979bbba7e
@alex-grechnyi <alexgrechnyi@gmail.com>
2019-07-12 12:19:52 +0300

    gulpfile configured

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    del = require('del'),
+    postcss = require("gulp-postcss"),
+    autoPrefix = require('autoprefixer'),
+    cssNano = require("cssnano");
+
+function html(){
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('./build'));
+}
+
+function style() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(gulp.dest('./src/css'))
+        .pipe(browserSync.stream());
+}
+
+function reload() {
+    browserSync.reload();
+}
+function watch(){
+    browserSync.init(
+        {
+            server: {
+                baseDir: "./src"
+            }
+        }
+    );
+    gulp.watch("./src/styles/*.scss", style);
+    gulp.watch("./src/*.html", reload);
+}
+
+function styleBuild() {
+    return gulp.src('./src/css/main.css')
+        .pipe(postcss([autoPrefix(), cssNano()]))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+function clean(){
+    return del('./build/*');
+}
+
+let build = gulp.series(clean, gulp.parallel(styleBuild, html));
+
+gulp.task('build', build);
+gulp.task('watch', watch);
